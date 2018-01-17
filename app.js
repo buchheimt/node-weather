af37bf29371f11a2176612e1924316aad74a3602
Tyler <tbuchhei@alumni.nd.edu>
2018-01-17 16:04:26 -0600

    Add error handling to google request

--- app.js
+++ app.js
@@ -14,13 +14,19 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-const address = encodeURIComponent(argv.address);
+const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (err, res, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`LAT: ${body.results[0].geometry.location.lat}`);
-  console.log(`LNG: ${body.results[0].geometry.location.lng}`);
+  if (err) {
+    console.log("Unable to connect to Google's servers");
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('No results match that address');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`LAT: ${body.results[0].geometry.location.lat}`);
+    console.log(`LNG: ${body.results[0].geometry.location.lng}`);
+  }
 });
