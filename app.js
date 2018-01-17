8765227de23d7fa990e116a3c5b516532a99a506
Tyler <tbuchhei@alumni.nd.edu>
2018-01-17 15:53:53 -0600

    Add yargs and fetch location based on user input

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const address = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1750%20FM%20423%20Frisco',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (err, res, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
